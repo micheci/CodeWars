45ec4d28d706d314e54ed3d9d5ffa84d5129853f
micheci <85264218+micheci@users.noreply.github.com>
2023-05-06 15:22:24 -0500

    Create Cascading Subsets.js

---
+++ Cascading Subsets.js
@@ -0,0 +1,10 @@
+function eachCons(array, n) {
+  let res=[]
+
+  
+   for(let i = 0; i < array.length; i++){
+    res.push(array.slice(i,i + n));
+  }
+  
+	return res.filter(e => e.length === n);
+}
